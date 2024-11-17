3bacb1a5b08a969670621993a337ced0ec2e9836
kvardaan <vardaankhattar8907@gmail.com>
2024-11-17 16:00:57 +0530

    Feat: Added root

---
+++ root/src/App.tsx
@@ -0,0 +1,22 @@
+function App() {
+	return (
+		<main className="flex flex-col gap-y-6 items-center justify-center h-screen text-xl font-semibold">
+			<a
+				href="https://roc8-moonshot-assignment-q1.vercel.app"
+				target="_blank"
+				className="underline underline-offset-2 decoration-blue-400"
+			>
+				Q1 (Outlook like Application)
+			</a>
+			<a
+				href="https://roc8-moonshot-assignment-seven.vercel.app"
+				target="_blank"
+				className="underline underline-offset-2 decoration-blue-400"
+			>
+				Q2 (Interactive Data Visualization Dashboard)
+			</a>
+		</main>
+	);
+}
+
+export default App;
